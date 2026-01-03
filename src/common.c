dfdbdfa566851f3030d6453baccc0a009b9a347a
Kotaro Noguchi <kotaro.noguchi@linecorp.com>
2026-01-03 20:43:49 +0900

    re-impl checksum

--- src/common.c
+++ src/common.c
@@ -11,14 +11,21 @@ void *memcpy(void *dest, const void *src, uint16_t len)
 }
 
 uint16_t checksum(void* bytes, uint16_t len) {
-    uint16_t *pos = (uint16_t*)bytes;
-    unsigned long sum = 0;
-    for (int i = 0; i < len / 2 + len % 1; i++) {
-        printf("adding %X from %X. i: %d\n", *pos, pos, i);
-        sum += *pos;
-        pos++;
+    const uint8_t *p = (const uint8_t *)bytes;
+    uint32_t sum = 0;
+    while (len >= 2) {
+        uint16_t word = ((uint16_t)p[0] << 8) | (uint16_t)p[1];
+        printf("adding %X from p: %X", word, p);
+        sum += word;
+        p += 2;
+        len -= 2;
     }
-    sum = (sum & 0xFFFF) + (sum >> 16);
-    sum = (sum & 0xFFFF) + (sum >> 16);
-    return ~sum;
+    if (len == 1) {
+        sum += (uint16_t)p[0] << 8;
+    }
+
+    while (sum >> 16) {
+        sum = (sum & 0xFFFF) + (sum >> 16);
+    }
+    return (uint16_t)~sum;
 }
